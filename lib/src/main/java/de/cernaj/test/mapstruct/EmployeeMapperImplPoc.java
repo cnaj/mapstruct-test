4a01cfc7797e25d50049c2edae160b9fef08cd4c
Tomas Cernaj <tomas@cernaj.de>
2023-04-25 21:27:10 +0200

    Change to constant fields

--- lib/src/main/java/de/cernaj/test/mapstruct/EmployeeMapperImplPoc.java
+++ lib/src/main/java/de/cernaj/test/mapstruct/EmployeeMapperImplPoc.java
@@ -1,18 +1,36 @@
 package de.cernaj.test.mapstruct;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 public class EmployeeMapperImplPoc implements EmployeeMapper {
     public static final EmployeeMapperImplPoc INSTANCE = new EmployeeMapperImplPoc();
 
-    private static final Confidential[] SOURCE_ANNOTATIONS_CONFIDENTIAL;
+    private final Confidential addressConfidentialAnnotation;
+    private final Confidential salaryConfidentialAnnotation;
 
-    static {
+    public EmployeeMapperImplPoc() {
+        addressConfidentialAnnotation = getMethodAnnotation( Confidential.class, "getAddress" );
+        salaryConfidentialAnnotation = getMethodAnnotation( Confidential.class, "getSalary" );
+    }
+
+    private static <T extends Annotation> T getMethodAnnotation(Class<T> annotationClass, String methodName) {
         try {
-            SOURCE_ANNOTATIONS_CONFIDENTIAL = new Confidential[] {
-                Employee.class.getMethod( "getAddress" ).getAnnotation( Confidential.class ),
-                Employee.class.getMethod( "getSalary" ).getAnnotation( Confidential.class ),
-            };
+            Method method = Employee.class.getMethod( methodName );
+            return method.getAnnotation( annotationClass );
         }
         catch ( NoSuchMethodException e ) {
+            throw new RuntimeException( e );
+        }
+    }
+
+    private static <T extends Annotation> T getFieldAnnotation(Class<T> annotationClass, String fieldName) {
+        try {
+            Field field = Employee.class.getField( fieldName );
+            return field.getAnnotation( annotationClass );
+        }
+        catch ( NoSuchFieldException e ) {
             throw new RuntimeException( e );
         }
     }
@@ -28,8 +46,8 @@ public class EmployeeMapperImplPoc implements EmployeeMapper {
         String salary = null;
 
         name = mapProperty( value.getName(), accessLevel, null );
-        address = mapProperty( value.getAddress(), accessLevel, SOURCE_ANNOTATIONS_CONFIDENTIAL[0] );
-        salary = mapProperty( value.getSalary(), accessLevel, SOURCE_ANNOTATIONS_CONFIDENTIAL[1] );
+        address = mapProperty( value.getAddress(), accessLevel, addressConfidentialAnnotation );
+        salary = mapProperty( value.getSalary(), accessLevel, salaryConfidentialAnnotation );
 
         EmployeeDTO employeeDTO = new EmployeeDTO( name, address, salary );
 
